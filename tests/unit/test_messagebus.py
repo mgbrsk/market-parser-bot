2fa1dbaa4b690b7db102ca070f6894438190d4c2
Arseny Mironov <noname02@inbox.ru>
2025-02-16 18:43:03 +0500

    Add 1 unit test for bus.

---
+++ tests/unit/test_messagebus.py
@@ -0,0 +1,16 @@
+import pytest
+
+from src.bot.adapters.message import AbstractBusMessage
+from src.bot.service_layer.messagebus import MessageBus
+
+
+class FakeBusMessage(AbstractBusMessage):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_message_received_in_queue():
+    message_bus = MessageBus()
+    message: AbstractBusMessage = FakeBusMessage()
+    await message_bus.handle(message)
+    assert message_bus.queue == [message]
