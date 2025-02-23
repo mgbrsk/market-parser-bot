f18ac9878e1493a10400d3b95761242dc324aae4
Arseny Mironov <noname02@inbox.ru>
2025-02-23 18:16:02 +0500

    Add handlers template

--- src/bot/adapters/handlers.py
+++ src/bot/adapters/handlers.py
@@ -1,9 +1,23 @@
 import abc
 
-from src.bot.adapters.message import AbstractBusMessage
+from src.bot.adapters import message
+from src.bot.domain.model import MessageToUser
 
 
 class AbstractHandler(abc.ABC):
     @abc.abstractmethod
-    async def handle(self, message: AbstractBusMessage):
+    async def handle(self, message: message.AbstractBusMessage):
         raise NotImplementedError
+
+
+class SearchHandler(AbstractHandler):
+    async def handle(self, message: message.SearchRequest):
+        pass
+
+
+class TelegramHandler(AbstractHandler):
+    async def handle(self, message: message.AbstractBusMessage):
+        pass
+
+    async def send_message(self, message: MessageToUser):
+        pass
